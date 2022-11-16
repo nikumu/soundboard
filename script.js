e4525789c381dfb55e8f64299b5282092b3478f8
André Bianchi <andrecarlosbb@gmail.com>
2022-11-16 03:29:03 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const sounds = ['among','banido','counter-strike',
+    'goofy','wow','xaropinho']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
